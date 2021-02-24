9b0431b94e3f43ebdcbe965a3eb7681ad2503aa4
Jerrin Eldo Mazhuvancherry <jerin.jollybhavan@gmail.com>
2021-02-24 12:42:09 -0500

    Jerrin's changes for Login page

--- Pages/nav.php
+++ Pages/nav.php
@@ -23,7 +23,7 @@
         </ul>
         <ul class="nav navbar-nav navbar-right">
             <li><a href="#"><span class="glyphicon glyphicon-user"></span>Register</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li><a href="Login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
         </ul>
     </div>
 </nav>
