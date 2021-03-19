4331fe3d566620628906cf4bb484be3e65ec1811
Luis Botello <botello48@gmailcom>
2021-03-19 17:14:57 -0400

    Added dashboard page

--- Pages/dashboard.php
+++ Pages/dashboard.php
@@ -1,3 +1,23 @@
-<?php include 'header.php'; ?>
+<?php include 'header.php'; include 'nav.php';?>
+<link rel="stylesheet" href="../Stylesheets/reports_page.css">
 <!--TODO ADD INFO-->
+<div class="m-1">
+  <table class="table table-bordered tbl">
+    <thead>
+      <tr>
+        <th scope="col">Alert Priority</th>
+        <th scope="col">Alert Description</th>
+        <th scope="col">Soldier Name</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>10</td>
+        <td>Birthday</td>
+        <td>Tyler, Elliot</td><!-- Eventually will use names pulled from database -->
+        <td><a href="#" class="button btn btn-primary">Details</a></td><!-- Takes user to generated page with actions to resolve -->
+      </tr>
+    </tbody>
+  </table>
+</div>
 <?php include "footer.php"; ?>
