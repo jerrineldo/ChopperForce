7298c9be263e3ca84d35da85578b05e223674652
Asia Levesque Gault <asialevesquegault@gmail.com>
2021-03-19 13:46:23 -0600

    Add files via upload

---
+++ Pages/oer_report.php
@@ -0,0 +1,13 @@
+<?php
+    require_once 'header.php';
+?>
+    <style>
+        <?php
+            require_once '../Stylesheets/reports_page.css';
+        ?>
+    </style>
+<?php
+    require_once 'nav.php';
+    require_once './OER/oer_report_list.php';
+    require_once 'footer.php';
+?>
