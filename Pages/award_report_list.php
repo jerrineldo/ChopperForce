3fe93634f0eb6ce3e25d0958df2fd977198da816
Journeygault <71765640+Journeygault@users.noreply.github.com>
2021-03-19 14:25:13 -0400

    Add files via upload

---
+++ Pages/award_report_list.php
@@ -0,0 +1,65 @@
+
+<?php
+
+require 'header.php';
+require 'nav.php';
+require '../stylesheets/form.css';
+
+
+?>
+<!--
+Form By Journey
+    !-->
+<body>
+<link rel="stylesheet" href="../Stylesheets/form.css">
+<link rel="stylesheet" href="../Stylesheets/reports_page.css">
+
+<h1 class="report-title">Awards Report Page</h1>
+<table class="table table-bordered tbl tbl__frg">
+    <thead>
+        <tr>
+            <th>Award Id</th>
+            <th>Award Name</th>
+            <th>Update</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>John H Merrit Award</td>
+            <td>
+                <form action="" method="post">
+                    <input type="hidden" name="id" value=""/>
+                    <input type="submit" class="button btn btn-primary" name="updateAward" value="Update"/>
+                </form>
+            </td>
+            <td>
+                <form action="" method="post">
+                    <input type="hidden" name="id" value="">
+                    <input type="submit" class="button btn btn-danger" name="deleteAward" value="Delete"/>
+                </form>
+            </td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Advanced flying awards</td>
+            <td>
+                <form action="" method="post">
+                    <input type="hidden" name="id" value=""/>
+                    <input type="submit" class="button btn btn-primary" name="updateAward" value="Update"/>
+                </form>
+            </td>
+            <td>
+                <form action="" method="post">
+                    <input type="hidden" name="id" value="">
+                    <input type="submit" class="button btn btn-danger" name="deleteAward" value="Delete"/>
+                </form>
+            </td>
+        </tr>
+    </tbody>
+</table>
+</body>
+<?php
+require 'footer.php';
+?>
