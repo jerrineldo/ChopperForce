5adb098a358f9a48e3be040a88aecfff6ce1edfe
asia gault <asialevesquegault@gmail.com>
2021-04-16 09:46:38 -0600

    practicing home

--- Pages/body_home.php
+++ Pages/body_home.php
@@ -8,9 +8,9 @@
 $dbcon= DatabaseContext::dbConnect();//DatabaseContext
 $s = new Oer();
 $oers = $s->getAllUpcommingOers(DatabaseContext::dbConnect());
-
 $s = new Ncoer();
 $ncoers = $s->getUpcommingNcoers(DatabaseContext::dbConnect());
+
 ?>
 <div class="row">
     <div class="col-sm-6">
@@ -42,10 +42,10 @@ $ncoers = $s->getUpcommingNcoers(DatabaseContext::dbConnect());
         </div>
     </div>
 </div>
-<h2 class="report-title">Upcoming OER</h2>
-<div class="m-1">
+
+
     
-    <table class="table table-bordered tbl">
+<!--    <table class="table table-bordered tbl">
         <thead>
         <tr>
             <th scope="col">OER_ID</th>
@@ -53,38 +53,52 @@ $ncoers = $s->getUpcommingNcoers(DatabaseContext::dbConnect());
             <th scope="col">First Name</th>
             <th scope="col">Last Name</th>
             <th scope="col">Rater</th>
-            <th scope="col">Int Rater</th>
-            <th scope="col">Senior Rater</th>
-            <th scope="col">Last OER</th>
-            <th scope="col">THRU Date</th>
             <th scope="col">Due</th>
-            <th scope="col">Type</th>
-            <th scope="col">Remarks</th>
         </tr>
         </thead>
         <tbody>
-        <?php foreach ($oers as $oer) { ?>
+        <?php /*foreach ($oers as $oer) { */?>
         <tr>
-            
-            <th><?= $oer->id; ?></th>
-            <th><?= $oer->rank; ?></th>
-            <th><?= $oer->first_name; ?></th>
-            <th><?= $oer->last_name; ?></th>
-            <th><?= $oer->rater; ?></th>
-            <th><?= $oer->int_rater; ?></th>
-            <th><?= $oer->senior_rater; ?></th>
-            <th><?= $oer->last_oer; ?></th>
-            <th><?= $oer->thru_date; ?></th>
-            <th><?= $oer->due; ?></th>
-            <th><?= $oer->type; ?></th>
-            <th><?= $oer->remarks; ?></th>
-        <?php } ?>
+            <th><?/*= $oer->id; */?></th>
+            <th><?/*= $oer->rank; */?></th>
+            <th><?/*= $oer->first_name; */?></th>
+            <th><?/*= $oer->last_name; */?></th>
+            <th><?/*= $oer->rater; */?></th>
+            <th><?/*= $oer->due; */?></th>
+        <?php /*} */?>
         </tbody>
-    </table>
+    </table>-->
     <div class="m-1">
-    <h2 class="report-title">Upcoming NCOER</h2>
+    <h4 id="rosterTitle"  class="report-title">Upcoming NCOER</h4>
 
-    <table class="table table-bordered tbl">
+        <table class="table" style="color:#9d9d9d; background-color:#fff;">
+            <thead>
+            <tr class="table-light">
+                <th scope="col">OER_ID</th>
+                <th scope="col">Rank </th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Rater</th>
+                <th scope="col">Due</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($oers as $oer) {?>
+            <tr class="table-white">
+                <th><?= $oer->id;?></th>
+                <th><?= $oer->rank;?></th>
+                <th><?= $oer->first_name;?></th>
+                <th><?= $oer->last_name;?></th>
+                <th><?= $oer->rater;?></th>
+                <th><?= $oer->due;?></th>
+            </tr>
+            <?php }?>
+            </tbody>
+        </table>
+
+
+
+<!--    <table class="table table-bordered tbl">
         <thead>
         <tr>
             <th scope="col">NCOER_ID</th>
@@ -102,25 +116,25 @@ $ncoers = $s->getUpcommingNcoers(DatabaseContext::dbConnect());
         </tr>
         </thead>
         <tbody>
-        <?php foreach ($ncoers as $ncoer) { ?>
+        <?php /*foreach ($ncoers as $ncoer) { */?>
             <tr>
-                <th><?= $ncoer->id; ?></th>
-                <th><?= $ncoer->rank; ?></th>
-                <th><?= $ncoer->first_name; ?></th>
-                <th><?= $ncoer->last_name; ?></th>
-                <th><?= $ncoer->rater; ?></th>
-                <th><?= $ncoer->senior_rater; ?></th>
-                <th><?= $ncoer->reviewer; ?></th>
-                <th><?= $ncoer->last_ncoer; ?></th>
-                <th><?= $ncoer->thru_date; ?></th>
-                <th><?= $ncoer->due; ?></th>
-                <th><?= $ncoer->type; ?></th>
-                <th><?= $ncoer->remarks; ?></th>
+                <th><?/*= $ncoer->id; */?></th>
+                <th><?/*= $ncoer->rank; */?></th>
+                <th><?/*= $ncoer->first_name; */?></th>
+                <th><?/*= $ncoer->last_name; */?></th>
+                <th><?/*= $ncoer->rater; */?></th>
+                <th><?/*= $ncoer->senior_rater; */?></th>
+                <th><?/*= $ncoer->reviewer; */?></th>
+                <th><?/*= $ncoer->last_ncoer; */?></th>
+                <th><?/*= $ncoer->thru_date; */?></th>
+                <th><?/*= $ncoer->due; */?></th>
+                <th><?/*= $ncoer->type; */?></th>
+                <th><?/*= $ncoer->remarks; */?></th>
             </tr>
 
-        <?php } ?>
+        <?php /*} */?>
         </tbody>
-    </table>
+    </table>-->
     <br>
     <br>
     <br>
