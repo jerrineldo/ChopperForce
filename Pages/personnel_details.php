68df8811f1db485d6bea3a70320b02ad7a6c592c
Luis Botello <botello468@gmail.com>
2021-04-20 21:54:11 -0400

    Finished logic for user details

--- Pages/personnel_details.php
+++ Pages/personnel_details.php
@@ -7,6 +7,11 @@
 <?php 
   require_once '../Models/DatabaseContext.php';
   require_once '../Models/User.php';
+  require_once '../Models/Award.php';
+  require_once '../Models/Dutie.php';
+  require_once '../Models/EmergencyContact.php';
+  require_once '../Models/FamilyContact.php';
+  require_once '../Models/PhysicalFitness.php';
   require_once './personal_report/details.php';
   include "footer.php"; 
 ?>
